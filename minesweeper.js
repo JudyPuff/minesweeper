838205ca2e0de493d5e04d437ae918391ee63d10
JudyPuff <judy.yun.ting@gmail.com>
2017-11-16 16:41:39 +1300

    added checkForWin function

--- minesweeper.js
+++ minesweeper.js
@@ -17,24 +17,49 @@ var board = {
 }
 
 function startGame () {
+  //loop through contents of each cell and then call countSurroundingMines
+let cell = 0; //start counting from cell 0
+  for (cell in board.cells) { //for every cell in the cells board
+    board.cells[cell].surroundingMines = countSurroundingMines(cell); //new object called surroundingMines is linked to the countSurroundingMines function below, and this applies to every cell
+  }
   // Don't remove this function call: it makes the game work!
-  lib.initBoard(document.getElementsByClassName('board')[0].children)
+  lib.initBoard(document.getElementsByClassName('board'))
+ document.addEventListener('click', checkForWin) //no need to do cell[i].document.addEventListener like the last game because we're not counting the cells, just the document in general
+ document.addEventListener('rightclick', checkForWin)
 }
 
 // Define this function to look for a win condition:
 //
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
-function checkForWin (cells) {
-  for (var i = 0; i < cells.length; i++){
-    cells[i].addEventListener('click', countSurroundingMines);
-    //if (isMine === false && hidden === false)
+function checkForWin () {
+
+//for each cell
+let cell = 0
+  for (cell in board.cells){
+//if the cell is a mine but is not marked the loop back again
+    if (board.cells[cell].isMarked){
+      return
+    }
   }
 
+// if cell is not a mine and is still not revealed then loop back again
+    if (board.cells[cell].isMine){
+      if (!board.cells[cell].isMine){
+    return
+  }
+}
+
+// if the cell is not a mine and not marked then loop back again
+if(board.cells[cell].hidden) {
+  if(!board.cells[cell].isMine){
+    return
+  }
+}
 
   // You can use this function call to declare a winner (once you've
   // detected that they've won, that is!)
-  lib.displayMessage('You win!')
+ lib.displayMessage('You win!')
 }
 
 // Define this function to count the number of mines around the cell
@@ -46,5 +71,13 @@ function checkForWin (cells) {
 // It will return cell objects in an array. You should loop through
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
-
+  var surroundingCells = lib.getSurroundingCells(board.cells[cell].row, board.cells[cell].col);
+  let count = 0
+  let square = undefined
+  for (square in surroundingCells) {
+    if (surroundingCells[square].isMine) {
+      count++
+    }
+  }
+  return count
 }
